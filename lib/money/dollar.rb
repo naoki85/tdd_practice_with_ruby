c3a0f00974865e7b12ef90baa27b56ab857455c7
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-28 13:57:38 +0900

    Add equals method to Dollar

--- lib/money/dollar.rb
+++ lib/money/dollar.rb
@@ -9,5 +9,9 @@ module Money
     def times(multiplier)
       Dollar.new(@amount * multiplier)
     end
+
+    def equals?(dollar)
+      dollar.kind_of?(Dollar) && @amount == dollar.amount
+    end
   end
 end
--- spec/lib/money/dollar_spec.rb
+++ spec/lib/money/dollar_spec.rb
@@ -10,4 +10,13 @@ describe 'Money::Dollar' do
       expect(product.amount).to eq 15
     end
   end
+
+  describe 'equals?' do
+    it do
+      expect(Money::Dollar.new(5).equals?(Money::Dollar.new(5))).to be_truthy
+      expect(Money::Dollar.new(5).equals?(Money::Dollar.new(6))).to be_falsey
+      DummyDollar = Struct.new(:amount)
+      expect(Money::Dollar.new(5).equals?(DummyDollar.new(5))).to be_falsey
+    end
+  end
 end
