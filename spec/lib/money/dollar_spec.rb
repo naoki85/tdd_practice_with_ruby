2526a5e80c164d2c5200ce49eefba003d1793557
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-28 13:30:32 +0900

    Fix times method with returning self

--- spec/lib/money/dollar_spec.rb
+++ spec/lib/money/dollar_spec.rb
@@ -4,8 +4,10 @@ describe 'Money::Dollar' do
   describe 'multiplication' do
     it do
       five_dollar = Money::Dollar.new(5)
-      five_dollar.times(2)
-      expect(five_dollar.amount).to eq 10
+      product = five_dollar.times(2)
+      expect(product.amount).to eq 10
+      product = five_dollar.times(3)
+      expect(product.amount).to eq 15
     end
   end
 end
